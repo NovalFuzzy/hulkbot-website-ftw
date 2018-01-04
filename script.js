3b5cdbcd705dc5bbc90d49498639c164d4fdf3d7
FHGDev <gideon.lingle14@gmail.com>
2018-01-04 16:40:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-var Devs = [localStorage.getItem("Dev1"), localStorage.getItem("Dev2"), localStorage.getItem("Dev3")]
-
 localStorage.setItem("Dev1", "Hulk")
 localStorage.setItem("Dev2", "RHG")
 localStorage.setItem("Dev3", "NovalFuzzy")
+
+const dev1 = localStorage.getItem("Dev1")
+const dev2 = localStorage.getItem("Dev2")
+const dev3 = localStorage.getItem("Dev3")
+
+var Devs = [dev1, dev2, dev3]
 
 window.onload = function () {
   console.log(`All Devs for this project are ${Devs}`)
