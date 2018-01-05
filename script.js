6f8d075dc085799d8129849ab0f750c5b7bccce5
RHG <huskey.rusty@gmail.com>
2018-01-04 20:39:18 -0500

    dang son

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 localStorage.setItem("Dev1", "Hulk")
 localStorage.setItem("Dev2", " RHG ")
-localStorage.setItem("Dev3", " and NovalFuzzy")
+localStorage.setItem("Dev3", "and NovalFuzzy")
 
 const dev1 = localStorage.getItem("Dev1")
 const dev2 = localStorage.getItem("Dev2")
 const dev3 = localStorage.getItem("Dev3")
 
-var Devs = [dev1 + " ", dev2 + " ", dev3 + " "]
+var Devs = [dev1+dev2+dev3]
 
 window.onload = function () {
   console.log(`All Devs for this project are ${Devs}`)
